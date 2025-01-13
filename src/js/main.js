4b9b66104a34cfe2f261a23ff43eeb4062a95f44
Mürsel SEN <murselsen_35@hotmail.com>
2025-01-13 23:57:21 +0300

    "Deleted file: index"

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,55 @@
-let calValue, oneNumber, action, twoNumber, totalNumber;
+const calValueSpan = document.querySelector('#calValue');
+const oneNumberSpan = document.querySelector('#oneNumber');
+const processSpan = document.querySelector('#process');
+const twoNumberSpan = document.querySelector('#twoNumber');
+let calValue = 0,
+  oneNumber,
+  action,
+  twoNumber,
+  totalNumber,
+  numbers = [],
+  process = [];
 
-document.getElementById('allDelete').addEventListener('click', function () {
-  alert('Calculator Cleared');
+const setCalValue = value => {
+  if (calValue === 0) {
+    calValue = value;
+  } else {
+    calValue.toString();
+    if (calValue.length < 8) {
+      calValue += value;
+    } else {
+      calValue = calValue.slice(1);
+      calValue += value;
+    }
+  }
+  document.getElementById('calValue').innerText = calValue;
+};
+const setAction = action => {
+  oneNumber = calValue;
+  oneNumberSpan.style.display = 'block';
+  document.querySelector('#oneNumber').style.display = 'block';
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('button');
+  console.log(buttons);
+  buttons.forEach((button, key) => {
+    if (button.className === 'number') {
+      numbers.push(button);
+    } else if (button.className === 'process') {
+      process.push(button);
+    }
+  });
+
+  numbers.forEach(number => {
+    number.addEventListener('click', e => {
+      // console.log(e.target.innerText);
+      setCalValue(e.target.innerText);
+    });
+  });
+  process.forEach(proc => {
+    proc.addEventListener('click', e => {
+      console.log(e.target.innerText);
+    });
+  });
 });
